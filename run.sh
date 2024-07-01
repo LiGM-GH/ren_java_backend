6c40f29b15881cfa19a780b2a96fb8db6190ed99
LiGM-GH <lipkin.grisha@mail.ru>
2024-07-01 23:40:48 +0300

    Fixed logging, moved daemon logs into main dir

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-export $(cat ./.env | grep -v ^# | xargs) >/dev/null
+export $(cat ./.env | grep -v ^# | xargs) >> /dev/null
 
 cd ./nsfw_lib/
 just release-build
-just release-daemon & \
-cd .. && \
+cd ..
+./nsfw_lib/target/release/nsfw_daemon & \
 NSFW_DAEMON_TCP_PORT=1248 \
 NSFW_DAEMON_TCP_HOST="localhost" \
 LD_LIBRARY_PATH="./lib" \
